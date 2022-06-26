ea959db24de01e5442dc895a895b7d3e0932874b
Pratik12350 <ss2983488@gmail.com>
2022-06-26 14:31:19 +0530

    added .searchUser() method

--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 # Nwift API
+
 ## A Pacakge to easily interact with nwift api
 
 # ❗ UNDERDEVELOPMENT STAGE! ❗
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,4 +1,4 @@
-const api = require("../src/index")
+const api = require("../src/index");
 
 /*
 test("Tests the 'testFunc' function", () => {
@@ -6,6 +6,15 @@ test("Tests the 'testFunc' function", () => {
 })
 */
 
-test("Tests the 'getUser' function", async() => {
-	expect(typeof (await api.getUser("nwift", "mum"))).toBe('object')
-})
+/*test("Tests the 'getUser' function", async() => {
+	expect(typeof (await api.getUser("snehasish", "bc8ef9dbf7c5ebdb138c4bf0b3f72fc0160fae6e03935099a210fd7b2ccebcb722caeeafcaa9c36eeed68a435033fe795a1dbc8ef9dbf7c5ebdb138c4bf0b3f72fc0160fae6e03935099a210fd7b2ccebcb722caeeafcaa9c36eeed68a435033fe795a1dbc8ef9dbf7c5ebdb138c4bf0b3f72fc0160fae6e03935099a210fd7b2ccebcb722caeeafcaa9c36eeed68a435033fe795a1d"))).toBe('object')
+})*/
+
+test("Tests the searchUser function", async () => {
+  expect(
+    typeof (await api.searchUser(
+      "s",
+      "bc8ef9dbf7c5ebdb138c4bf0b3f72fc0160fae6e03935099a210fd7b2ccebcb722caeeafcaa9c36eeed68a435033fe795a1dbc8ef9dbf7c5ebdb138c4bf0b3f72fc0160fae6e03935099a210fd7b2ccebcb722caeeafcaa9c36eeed68a435033fe795a1dbc8ef9dbf7c5ebdb138c4bf0b3f72fc0160fae6e03935099a210fd7b2ccebcb722caeeafcaa9c36eeed68a435033fe795a1d"
+    ))
+  ).toBe("object");
+});
